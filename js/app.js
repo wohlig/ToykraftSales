77baec5e6c6fb26b475624b0db48d1f9fe7dfbdc
Abhay Amin <pixoloproductions@gmail.com>
2014-09-25 14:12:38 +0530

    appjs ng

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var starter=angular.module('starter', ['ionic', 'starter.controllers', 'myservices'])
+var starter=angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'myservices'])
 
 .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
