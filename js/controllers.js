cea9e7e110f8c00cc641d5a2d34af6373a75bc58
Tushar <tushar@Macintosh-2.local>
2014-09-29 13:58:28 +0530

    fulllscreen false

--- js/controllers.js
+++ js/controllers.js
@@ -1108,7 +1108,7 @@ angular.module('starter.controllers', ['ngCordova', 'myservices'])
 
 })
 
-.controller('AddshopCtrl', function ($scope, $stateParams, $cordovaCamera, $cordovaProgressIndicator, $cordovaFile, $http, MyServices, $location) {
+.controller('AddshopCtrl', function ($scope, $stateParams, $cordovaCamera, $cordovaFile, $http, MyServices, $location) {
 
     var aid = $stateParams.areaid;
 
@@ -1200,7 +1200,6 @@ angular.module('starter.controllers', ['ngCordova', 'myservices'])
                     filenameee = result;
                     $scope.filename2 = result.file_name;
                     $scope.addretailer.store_image = $scope.filename2;
-                    $cordovaProgress.hide();
 
                 }, function (err) {
                     // Error
@@ -1208,7 +1207,6 @@ angular.module('starter.controllers', ['ngCordova', 'myservices'])
                     console.log("Error");
                 }, function (progress) {
                     // constant progress updates
-                    $cordovaProgress.showSimpleWithLabel(true, "Uploading image...");
                 });
 
         };
